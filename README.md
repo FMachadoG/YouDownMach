5ad989933dbf52534b462febb4f5bf204a502dff
Filipe Machado <filipemachado.g@gmail.com>
2022-08-21 01:29:25 -0300

    Update information in input, print and README

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-# YoutubeDownload
+# YouDownMach
 Youtube video and audio download
 
-Download "main.py" execute in terminal and voilà :)
+Download "main.py" execute in terminal and voilà :) 
+<br>
+**Max resolution is 720p**
+## Example:
+
+```
+PS C:\FMachado> python .\main.py
+
+######################################################
+##                    YouDownMach                   ##
+##          Execute in terminal and voilà :)        ##
+##      https://github.com/FMachadoG/YouDownMach    ##
+##          By: https://github.com/FMachadoG        ##
+######################################################
+
+ - URL video: https://youtu.be/Obx3e7MfEaI
+ - Download video?(y/n): y
+ - Download audio?(y/n): y
+ - Show video information?(y/n): y
+
+########## DOWNLOADING ##########
+ - Path to download: C:\Fmachado
+ - Video resolution lowest or highest?(l/h)): h
+
+ - Downloading video...
+ - Done! - 21/08/2022 01:24:59
+
+ - Downloading audio...
+ - Done! - 21/08/2022 01:24:59
+
+########## VIDEO INFORMATION ##########
+ - Title: --------- Finding Hope - Without You (feat. Holly Drummond)
+ - Views: --------- 4.930.387
+ - Publish date: -- Apr 05, 2016
+ - Author: -------- MrSuicideSheep
+ - Time: ---------- 00:03:45
+ - Channel: ------- https://www.youtube.com/channel/UC5nc_ZtjKW1htCVZVRxlQAQ
+ - Thumb: --------- https://i.ytimg.com/vi/Obx3e7MfEaI/sddefault.jpg
+
+########################################
+```
--- main.py
+++ main.py
@@ -1,102 +1,107 @@
 from time import sleep
 from pytube import YouTube
 import re
-from datetime import timedelta
+from datetime import datetime, timedelta
 import os
 
-######################################################
-##                      YouDownMach                 ##
-#           Execute in terminal and voilà :)        ##
-##      https://github.com/FMachadoG/YouDownMach    ##
-##          By: https://github.com/FMachadoG        ##
-######################################################
+print(
+    """######################################################\n"""
+    """##                    YouDownMach                   ##\n"""
+    """##          Execute in terminal and voilà :)        ##\n"""
+    """##      https://github.com/FMachadoG/YouDownMach    ##\n"""
+    """##          By: https://github.com/FMachadoG        ##\n"""
+    """######################################################\n"""
+    )
 
-link = input(f"Link video: ")
+link = input(" - URL video: ")
 
 linkCode = link.replace("https://www.youtube.com", "").replace("https://youtu.be", "")
 validLink = re.match(r"(?:v=|\/)([0-9A-Za-z_-]{11}).*", linkCode)
 
 if (link.__contains__('https://www.youtube.com') == False and link.__contains__('https://youtu.be') == False):
     print(f"Invalid URL!")
-    sleep(5)
+    
     exit()
 
 elif validLink is None:
-        print(f"Invalid CODE video!")
-        sleep(5)
+        print("Invalid CODE video!")
+        
         exit()
 
 yt = YouTube(link)
-downVideo = input(f"Download video?(y/n): ")
+downVideo = input(" - Download video?(y/n): ")
 
 if not (downVideo == 'y' or downVideo == 'n'):
-    print(f'Invalid!')
-    sleep(5)
+    print("Invalid answer! Please enter 'y'(yes) or 'n'(no).")
+    
     exit()
 
-downAudio = input(f"Download audio?(y/n): ")
+downAudio = input(" - Download audio?(y/n): ")
 
 if not (downAudio == 'y' or downAudio == 'n'):
-    print(f'Invalid!')
-    sleep(5)
+    print("Invalid answer! Please enter 'y'(yes) or 'n'(no).")
+    
     exit()
 
-showInfo = input(f"Show video information?(y/n): ")
+showInfo = input(" - Show video information?(y/n): ")
 
 if not (showInfo == 'y' or showInfo == 'n'):
-    print(f'Invalid!')
-    sleep(5)
+    print("Invalid answer! Please enter 'y'(yes) or 'n'(no).")
+    
     exit()
 
 if (downVideo == 'y' or downAudio == 'y'):
-    print(f"\n########## DOWNLOADING ##########")
+    print("\n########## DOWNLOADING ##########")
 
-    pathDownload = input('Path to donwload: ')
+    pathDownload = input(" - Path to download: ")
 
     if os.path.exists(pathDownload) == False:
         print("Path not exist!")
-        sleep(5)
+        
         exit()
 
     if downVideo == 'y':
-        resolVideo = input(f"Resolution video lowest or highest?(l/h)): ")
+        resolVideo = input(" - Video resolution lowest or highest?(l/h)): ")
 
         if not (resolVideo == 'l' or resolVideo == 'h'):
-            print(f"nvalid resolution!")
-            sleep(5)
+            print("Invalid answer! Please enter 'l'(lowest)' or 'h'(highest).")
+            
             exit()
 
         elif resolVideo == 'l':
-            print("\nDownloading video...")
+            print("\n - Downloading video...")
 
             yVideo = yt.streams.get_lowest_resolution()
             yVideoTitle = yVideo.title.replace(".", "_")
 
             yVideo.download(pathDownload, f'{yVideoTitle}-YouDownMach.mp4')
 
-            print(f"**Done!")
+            dateNow = datetime.now()
+            print(f" - Done! - {dateNow.strftime('%d/%m/%Y %H:%M:%S')}")
             
 
         else:
-            print("\nDownloading video...")
+            print("\n - Downloading video...")
 
             yVideo = yt.streams.get_highest_resolution()
             yVideoTitle = yVideo.title.replace(".", "_")
 
             yVideo.download(pathDownload, f'{yVideoTitle}-YouDownMach.mp4')
 
-            print(f"**Done!")
+            dateNow = datetime.now()
+            print(f" - Done! - {dateNow.strftime('%d/%m/%Y %H:%M:%S')}")
             
 
     if downAudio == 'y':
-        print("\nDownloading audio...")
+        print("\n - Downloading audio...")
 
         yAudio = yt.streams.get_audio_only("mp4")
         yAudioTitle = yAudio.title.replace(".", "_")
 
         yAudio.download(pathDownload, f'{yAudioTitle}-YouDownMach.mp3')
 
-        print(f"**Done!")
+        dateNow = datetime.now()
+        print(f" - Done! - {dateNow.strftime('%d/%m/%Y %H:%M:%S')}")
         
 
 if showInfo == 'y':
@@ -110,15 +115,15 @@ if showInfo == 'y':
     timeVideo = yt.length
     timeVideo = "{:0>8}".format(str(timedelta(seconds=timeVideo)))
 
-    print(f"\n########## VIDEO INFORMATION ##########")
+    print("\n########## VIDEO INFORMATION ##########")
 
-    print(f"Title: {yt.title}")
-    print(f"Views: {viewsAmountVideo}")   
-    print(f"Publish date: {publiDateVideo}")
-    print(f"Author: {yt.author}")
-    print(f"Time: {timeVideo}")
-    print(f"Channel: {yt.channel_url}")
-    print(f"Thumb: {yt.thumbnail_url}")
+    print(f" - Title: --------- {yt.title}")
+    print(f" - Views: --------- {viewsAmountVideo}")   
+    print(f" - Publish date: -- {publiDateVideo}")
+    print(f" - Author: -------- {yt.author}")
+    print(f" - Time: ---------- {timeVideo}")
+    print(f" - Channel: ------- {yt.channel_url}")
+    print(f" - Thumb: --------- {yt.thumbnail_url}")
 
     # print(f" Descrip: {yt.description}")
     # print(f" Keywords: {yt.keywords}")
